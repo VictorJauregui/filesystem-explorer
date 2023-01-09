aa141e8bc1e27ebfd1034024c3ec390a9934552e
Victor Jauregui <vicjaudom@hotmail.com>
2023-01-09 16:14:19 +0100

    update victor

--- index.php
+++ index.php
@@ -4,9 +4,71 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style1.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=IM+Fell+Great+Primer+SC&display=swap');
+    </style>
+
+    <title>System Explorer</title>
 </head>
 <body>
+    <div class="nav-bar">
+        <img class="logo" src="assets/logo.png" alt="logo"> 
+        <input type="search">
+        <div class="user-right">
+            <img class="icon-user"src="assets/icon-user.png" alt="user">
+        </div>
+        
+    </div>
+    <div class="all-screen">
+        <div class="sidebar">
+            <div class="div-btn-create">
+                <div class="btn-create">
+                    <img src="assets/icon-create.png" alt="create icon">
+                    <h2>Create</h2>
+                </div>
+            </div>
+            <div class="div-type-sidebar div-type-sidebar1">
+                <img class="icon-type" src="assets/icon-documents.png" alt="icon document">
+                <h2 class="h2-title-sidebar">Documents</h2>
+                <div class="div-down-arrow">
+                    <img class="down-arrow" src="assets/arrow-down.png" alt="arrow-down">
+                </div>
+            </div>
+            <div class="div-type-documents">
+                <img class="icon-type-documents" src="assets/icon-ppt.png" alt="icon power point">
+                <p class="text-type-document">Power point</p>
+            </div> 
+            <div class="div-type-documents">
+                <img class="icon-type-documents" src="assets/icon-ppt.png" alt="icon power point">
+                <p class="text-type-document">Power point</p>
+            </div>   
+            <div class="div-type-sidebar">
+                <img class="icon-type" src="assets/icon-images.png" alt="icon images">
+                <h2 class="h2-title-sidebar">Images</h2>
+                <div class="div-down-arrow">
+                    <img class="down-arrow" src="assets/arrow-down.png" alt="arrow-down">
+                </div> 
+            </div>
+            <div class="div-type-sidebar">
+                <img class="icon-type" src="assets/icon-audio-video.png" alt="icon audio-video">
+                <h2 class="h2-title-sidebar">Audio/Video</h2>
+                <div class="div-down-arrow">
+                    <img class="down-arrow" src="assets/arrow-down.png" alt="arrow-down">
+                </div> 
+            </div>
+            <div class="div-type-sidebar">
+                <img class="icon-type" src="assets/icon-trash.png" alt="icon document">
+                <h2 class="h2-title-sidebar">Rubish</h2>
+                <div class="div-down-arrow">
+                    <img class="down-arrow" src="assets/arrow-down.png" alt="arrow-down">
+                </div> 
+            </div>
+        </div>
+        <div class="content">
+            <h1>All</h1>
+        </div>
+    </div>
     
 </body>
 </html>
