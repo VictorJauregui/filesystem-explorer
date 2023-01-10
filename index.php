3c3b6860ddb350fc9beae85216953871579a1fde
Victor Jauregui <vicjaudom@hotmail.com>
2023-01-10 16:57:14 +0100

    update

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <script src="script.js" defer></script>
+    <script src="script1.js" defer></script>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=IM+Fell+Great+Primer+SC&display=swap');
     </style>
@@ -23,7 +23,7 @@
                 </div>
             </div>
             <div class="type-of-update">
-                <div class="card-type-update">
+                <div class="card-type-update card-directory">
                     <div class="div-icon-type-update">
                         <img class="icon-type-update" src="assets/icon-directory.png" alt="icon-directory">
                     </div>
@@ -40,11 +40,11 @@
     </div>
     <div class="modal2">
         <div class="contenido-modal2">
+            <div class="div-icon-close close-modal2">
+                <img class="icon-close icon-close2" src="assets/icon-close.png" alt="icon close">
+            </div>
             <div class="top-contenido-modal2">
-                <h2>NAME OF DICTORY</h2>
-                <div class="div-icon-close">
-                    <img class="icon-close" src="assets/icon-close.png" alt="icon close">
-                </div>
+                <h2 class="h2-modal2">NAME OF DIRECTORY</h2>
             </div>
             <input class="input-directory" type="text">
             <div class="buttons-modal2">
@@ -53,7 +53,7 @@
             </div>
         </div>
     </div>
-
+   <!-------------------DASHBOARD---------------------> 
     </div>
     <div class="nav-bar">
         <img class="logo" src="assets/logo.png" alt="logo"> 
@@ -175,11 +175,70 @@
                     </div>
                     <p>Lorem Ipsum is simply dummy text of the printing and</p>
                 </div>
+                <div class="card-document">
+                    <div class="div-icon-card">
+                        <img class="icon-card" src="assets/word-color.png" alt="icon power point">
+                    </div>
+                    <p>Lorem Ipsum is simply dummy text of the printing and</p>
+                </div>
+                <div class="card-document">
+                    <div class="div-icon-card">
+                        <img class="icon-card" src="assets/word-color.png" alt="icon power point">
+                    </div>
+                    <p>Lorem Ipsum is simply dummy text of the printing and</p>
+                </div>
+                <div class="card-document">
+                    <div class="div-icon-card">
+                        <img class="icon-card" src="assets/word-color.png" alt="icon power point">
+                    </div>
+                    <p>Lorem Ipsum is simply dummy text of the printing and</p>
+                </div>
+                <div class="card-document">
+                    <div class="div-icon-card">
+                        <img class="icon-card" src="assets/word-color.png" alt="icon power point">
+                    </div>
+                    <p>Lorem Ipsum is simply dummy text of the printing and</p>
+                </div>
+                <div class="card-document">
+                    <div class="div-icon-card">
+                        <img class="icon-card" src="assets/word-color.png" alt="icon power point">
+                    </div>
+                    <p>Lorem Ipsum is simply dummy text of the printing and</p>
+                </div>
                 
               
-            </div>
-            
+            </div>  
         </div>
+        <div class="content-individual-document">
+            <div class="div-icon-close-right">
+                <img class="icon-close icon-close-right-part" src="assets/icon-close.png" alt="icon close">
+            </div>
+            <div class="top-type-document">
+                <div class="div-individual-content">
+                    <h2>TITLE</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing</p>
+                </div>   
+                <div class="div-individual-content">
+                    <h2>CREATION DATE</h2>
+                    <p>02-01-2023</p>
+                </div>  
+                <div class="div-individual-content">
+                    <h2>LAST MODIFIED DATE</h2>
+                    <p>05-01-2023</p>
+                </div>  
+                <div class="div-individual-content">
+                    <h2>TYPE</h2>
+                    <div class="icon-and-name">
+                        <img class="icon-type-document" src="assets/word-color.png" alt="type of document">
+                        <p>Microsoft Word</p>
+                    </div>
+                </div>
+            </div>
+            <div class="div-logo2">
+                <img class="logo2" src="assets/logo2.png" alt="logo secundary">
+            </div>
+        </div>
+        
     </div>
     
 </body>
