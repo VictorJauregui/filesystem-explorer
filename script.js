3c3a580ceb807406c1da9a47988150677d71149a
Giorgi Meshveliani <xgorgaslax@gmail.com>
2023-01-10 09:18:25 +0100

    js v 1

--- script.js
+++ script.js
@@ -17,8 +17,30 @@ function openDocuments(){
     documents.style.marginRight = "10px";
     typeOfDocuments.style.marginLeft = "10px";
     typeOfDocuments.style.marginRight = "10px";
+
+    if(typeOfDocuments.classList.contains("type-of-documents")){
+        documents.style.backgroundColor = "#DCF2FF"
+        documents.style.marginLeft = "0px";
+        documents.style.marginRight = "0px";
+        typeOfDocuments.style.marginLeft = "0px";
+        typeOfDocuments.style.marginRight = "0px";
+    }
 }
 
 function openImages(){
-    typeOfImages.classList.toggle("type-of-images")
+    typeOfImages.classList.toggle("type-of-images");
+    typeOfImages.style.backgroundColor = "#8BD5FF";
+    images.style.backgroundColor= "#8BD5FF";
+    images.style.marginLeft = "10px";
+    images.style.marginRight = "10px";
+    typeOfImages.style.marginLeft = "10px";
+    typeOfImages.style.marginRight = "10px";
+
+    if(typeOfImages.classList.contains("type-of-images")){
+        images.style.backgroundColor = "#DCF2FF"
+        images.style.marginLeft = "0px";
+        images.style.marginRight = "0px";
+        typeOfImages.style.marginLeft = "0px";
+        typeOfImages.style.marginRight = "0px";
+    }
 }
