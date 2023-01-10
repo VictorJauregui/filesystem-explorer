74e99c1c0679c6425ae658191a8976926ced86f7
Victor Jauregui <vicjaudom@hotmail.com>
2023-01-10 12:00:42 +0100

    update

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style1.css">
-    <script src="script1.js" defer></script>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js" defer></script>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=IM+Fell+Great+Primer+SC&display=swap');
     </style>
@@ -13,6 +13,48 @@
     <title>System Explorer</title>
 </head>
 <body>
+    <!-------------------MODAL---------------------> 
+    <div class="modal">
+        <div class="contenido-modal">
+            <div class="div-h2-modal">
+                <h2 class="h2-modal">Update</h2>
+                <div class="div-icon-close">
+                    <img class="icon-close" src="assets/icon-close.png" alt="icon close">
+                </div>
+            </div>
+            <div class="type-of-update">
+                <div class="card-type-update">
+                    <div class="div-icon-type-update">
+                        <img class="icon-type-update" src="assets/icon-directory.png" alt="icon-directory">
+                    </div>
+                    <p>DIRECTORY</p>
+                </div>
+                <div class="card-type-update">
+                    <div class="div-icon-type-update">
+                        <img class="icon-type-update" src="assets/icon-file.png" alt="icon-directory">
+                    </div>
+                    <p>FILE</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal2">
+        <div class="contenido-modal2">
+            <div class="top-contenido-modal2">
+                <h2>NAME OF DICTORY</h2>
+                <div class="div-icon-close">
+                    <img class="icon-close" src="assets/icon-close.png" alt="icon close">
+                </div>
+            </div>
+            <input class="input-directory" type="text">
+            <div class="buttons-modal2">
+                <button class="button-secundary">BACK</button>
+                <button class="button-primary">CREATE</button>
+            </div>
+        </div>
+    </div>
+
+    </div>
     <div class="nav-bar">
         <img class="logo" src="assets/logo.png" alt="logo"> 
         <input type="search">
@@ -29,86 +71,99 @@
                     <h2>Create</h2>
                 </div>
             </div>
-            <div class="div-type-sidebar div-type-sidebar1 div-document">
-                <img class="icon-type" src="assets/icon-documents.png" alt="icon document">
-                <h2 class="h2-title-sidebar">Documents</h2>
-                <div class="div-down-arrow">
-                    <img class="down-arrow" src="assets/arrow-down.png" alt="arrow-down">
-                    <img class="down-arrow up-arrow" src="assets/icon-arrow-up.png" alt="arrow-down">
+            <div class="all-sidebar-documents">
+                <div class="div-type-sidebar div-type-sidebar1 div-document">
+                    <img class="icon-type" src="assets/icon-documents.png" alt="icon document">
+                    <h2 class="h2-title-sidebar">Documents</h2>
+                    <div class="div-down-arrow">
+                        <img class="down-arrow" src="assets/arrow-down.png" alt="arrow-down">
+                        <img class="down-arrow up-arrow" src="assets/icon-arrow-up.png" alt="arrow-down">
+                    </div>
+                </div>
+                <div class="type-of-documents">
+                    <div class="div-type-documents">
+                        <img class="icon-type-documents" src="assets/icon-ppt.png" alt="icon power point">
+                        <p class="text-type-document">POWER POINT</p>
+                    </div> 
+                    <div class="div-type-documents">
+                        <img class="icon-type-documents" src="assets/icon-word.png" alt="icon word">
+                        <p class="text-type-document">WORD</p>
+                    </div>  
+                    <div class="div-type-documents">
+                        <img class="icon-type-documents" src="assets/icon-csv.png" alt="icon csv">
+                        <p class="text-type-document">CSV</p>
+                    </div>  
+                    <div class="div-type-documents">
+                        <img class="icon-type-documents" src="assets/icon-odt.png" alt="icon odt">
+                        <p class="text-type-document">ODT</p>
+                    </div>  
+                    <div class="div-type-documents">
+                        <img class="icon-type-documents" src="assets/icon-pptt.png" alt="icon ppt">
+                        <p class="text-type-document">PPT</p>
+                    </div>  
+                    <div class="div-type-documents">
+                        <img class="icon-type-documents" src="assets/icon-txt.png" alt="icon txt">
+                        <p class="text-type-document">TXT</p>
+                    </div>  
+                    <div class="div-type-documents">
+                        <img class="icon-type-documents" src="assets/icon-exe.png" alt="icon exe">
+                        <p class="text-type-document">EXE</p>
+                    </div> 
+                    <div class="div-type-documents">
+                        <img class="icon-type-documents" src="assets/icon-zip.png" alt="icon zip">
+                        <p class="text-type-document">ZIP</p>
+                    </div> 
+                    <div class="div-type-documents">
+                        <img class="icon-type-documents" src="assets/icon-rar.png" alt="icon rar">
+                        <p class="text-type-document">RAR</p>
+                    </div> 
                 </div>
             </div>
-            <div class="type-of-documents">
-                <div class="div-type-documents">
-                    <img class="icon-type-documents" src="assets/icon-ppt.png" alt="icon power point">
-                    <p class="text-type-document">POWER POINT</p>
-                </div> 
-                <div class="div-type-documents">
-                    <img class="icon-type-documents" src="assets/icon-word.png" alt="icon power point">
-                    <p class="text-type-document">WORD</p>
-                </div>  
-                <div class="div-type-documents">
-                    <img class="icon-type-documents" src="assets/icon-csv.png" alt="icon power point">
-                    <p class="text-type-document">CSV</p>
-                </div>  
-                <div class="div-type-documents">
-                    <img class="icon-type-documents" src="assets/icon-odt.png" alt="icon power point">
-                    <p class="text-type-document">ODT</p>
-                </div>  
-                <div class="div-type-documents">
-                    <img class="icon-type-documents" src="assets/icon-pptt.png" alt="icon power point">
-                    <p class="text-type-document">PPT</p>
-                </div>  
-                <div class="div-type-documents">
-                    <img class="icon-type-documents" src="assets/icon-txt.png" alt="icon power point">
-                    <p class="text-type-document">TXT</p>
-                </div>  
-                <div class="div-type-documents">
-                    <img class="icon-type-documents" src="assets/icon-exe.png" alt="icon power point">
-                    <p class="text-type-document">EXE</p>
-                </div> 
-                <div class="div-type-documents">
-                    <img class="icon-type-documents" src="assets/icon-zip.png" alt="icon power point">
-                    <p class="text-type-document">ZIP</p>
-                </div> 
-                <div class="div-type-documents">
-                    <img class="icon-type-documents" src="assets/icon-rar.png" alt="icon power point">
-                    <p class="text-type-document">RAR</p>
-                </div> 
+            <div class="all-sidebar-images">
+                <div class="div-type-sidebar div-images">
+                    <img class="icon-type" src="assets/icon-images.png" alt="icon images">
+                    <h2 class="h2-title-sidebar">Images</h2>
+                    <div class="div-down-arrow">
+                        <img class="down-arrow" src="assets/arrow-down.png" alt="arrow-down">
+                    </div> 
+                </div>
+                <div class="type-of-images">
+                    <div class="div-type-images">
+                        <img class="icon-type-documents" src="assets/icon-jpg.png" alt="icon jpg">
+                        <p class="text-type-document">JPG</p>
+                    </div> 
+                    <div class="div-type-images">
+                        <img class="icon-type-documents" src="assets/icon-png.png" alt="icon png">
+                        <p class="text-type-document">PNG</p>
+                    </div> 
+                    <div class="div-type-images">
+                        <img class="icon-type-documents" src="assets/icon-jpg.png" alt="icon svg">
+                        <p class="text-type-document">SVG</p>
+                    </div> 
+                </div>
             </div>
-            <div class="div-type-sidebar div-images">
-                <img class="icon-type" src="assets/icon-images.png" alt="icon images">
-                <h2 class="h2-title-sidebar">Images</h2>
-                <div class="div-down-arrow">
-                    <img class="down-arrow" src="assets/arrow-down.png" alt="arrow-down">
-                </div> 
-            </div>
-            <div class="type-of-images">
-                <div class="div-type-images">
-                    <img class="icon-type-documents" src="assets/icon-jpg.png" alt="icon power point">
-                    <p class="text-type-document">JPG</p>
-                </div> 
-                <div class="div-type-images">
-                    <img class="icon-type-documents" src="assets/icon-png.png" alt="icon power point">
-                    <p class="text-type-document">PNG</p>
-                </div> 
-                <div class="div-type-images">
-                    <img class="icon-type-documents" src="assets/icon-jpg.png" alt="icon power point">
-                    <p class="text-type-document">SVG</p>
-                </div> 
-            </div>
-            <div class="div-type-sidebar">
-                <img class="icon-type" src="assets/icon-audio-video.png" alt="icon audio-video">
-                <h2 class="h2-title-sidebar">Audio/Video</h2>
-                <div class="div-down-arrow">
-                    <img class="down-arrow" src="assets/arrow-down.png" alt="arrow-down">
-                </div> 
+            <div class="all-sidebar-audio-video">
+                <div class="div-type-sidebar div-audio-video">
+                    <img class="icon-type" src="assets/icon-audio-video.png" alt="icon audio-video">
+                    <h2 class="h2-title-sidebar">Audio/Video</h2>
+                    <div class="div-down-arrow">
+                        <img class="down-arrow" src="assets/arrow-down.png" alt="arrow-down">
+                    </div> 
+                </div>
+                <div class="type-of-audio-video">
+                    <div class="div-type-audio-video">
+                        <img class="icon-type-documents" src="assets/icon-mp3.png" alt="icon mp3">
+                        <p class="text-type-document">MP3</p>
+                    </div> 
+                    <div class="div-type-audio-video">
+                        <img class="icon-type-documents" src="assets/icon-mp4.png" alt="icon mp4">
+                        <p class="text-type-document">MP4</p>
+                    </div> 
+                </div>
             </div>
             <div class="div-type-sidebar">
                 <img class="icon-type" src="assets/icon-trash.png" alt="icon document">
                 <h2 class="h2-title-sidebar">Rubish</h2>
-                <div class="div-down-arrow">
-                    <img class="down-arrow" src="assets/arrow-down.png" alt="arrow-down">
-                </div> 
             </div>
         </div>
         <div class="content">
--- script.js
+++ script.js
@@ -1,46 +1,61 @@
 const typeOfDocuments = document.querySelector(".type-of-documents");
 const typeOfImages = document.querySelector(".type-of-images")
+const typeOfAudioVideo = document.querySelector(".type-of-audio-video")
 const documents = document.querySelector(".div-document");
 const images = document.querySelector(".div-images")
+const audioVideo = document.querySelector(".div-audio-video")
 const upArrow = document.querySelector(".up-arrow");
 const downArrow = document.querySelector(".down-arrow");
+const allDocuments = document.querySelector(".all-sidebar-documents");
+const allImages = document.querySelector(".all-sidebar-images");
+const allAudioVideo = document.querySelector(".all-sidebar-audio-video");
 
 
 documents.addEventListener("click", openDocuments);
 images.addEventListener("click", openImages);
+audioVideo.addEventListener("click", openAudioVideo);
 
 function openDocuments(){
     typeOfDocuments.classList.toggle("type-of-documents");
-    typeOfDocuments.style.backgroundColor = "#8BD5FF";
-    documents.style.backgroundColor= "#8BD5FF";
-    documents.style.marginLeft = "10px";
-    documents.style.marginRight = "10px";
-    typeOfDocuments.style.marginLeft = "10px";
-    typeOfDocuments.style.marginRight = "10px";
+    allDocuments.style.backgroundColor= "#8BD5FF";
+    allDocuments.style.marginLeft = "10px";
+    allDocuments.style.marginRight = "10px";
+    allDocuments.style.borderRadius = "10px";
+
+
 
     if(typeOfDocuments.classList.contains("type-of-documents")){
-        documents.style.backgroundColor = "#DCF2FF"
-        documents.style.marginLeft = "0px";
-        documents.style.marginRight = "0px";
-        typeOfDocuments.style.marginLeft = "0px";
-        typeOfDocuments.style.marginRight = "0px";
+        allDocuments.style.backgroundColor = "#DCF2FF"
+        allDocuments.style.marginLeft = "0px";
+        allDocuments.style.marginRight = "0px";
     }
 }
 
 function openImages(){
     typeOfImages.classList.toggle("type-of-images");
-    typeOfImages.style.backgroundColor = "#8BD5FF";
-    images.style.backgroundColor= "#8BD5FF";
-    images.style.marginLeft = "10px";
-    images.style.marginRight = "10px";
-    typeOfImages.style.marginLeft = "10px";
-    typeOfImages.style.marginRight = "10px";
+    allImages.style.backgroundColor= "#8BD5FF";
+    allImages.style.marginLeft = "10px";
+    allImages.style.marginRight = "10px";
+    allImages.style.borderRadius = "10px";
+
 
     if(typeOfImages.classList.contains("type-of-images")){
-        images.style.backgroundColor = "#DCF2FF"
-        images.style.marginLeft = "0px";
-        images.style.marginRight = "0px";
-        typeOfImages.style.marginLeft = "0px";
-        typeOfImages.style.marginRight = "0px";
+        allImages.style.backgroundColor = "#DCF2FF"
+        allImages.style.marginLeft = "0px";
+        allImages.style.marginRight = "0px";
+    }
+}
+
+function openAudioVideo(){
+    typeOfAudioVideo.classList.toggle("type-of-audio-video");
+    allAudioVideo.style.backgroundColor = "#8BD5FF";
+    allAudioVideo.style.marginLeft = "10px";
+    allAudioVideo.style.marginRight = "10px";
+    allAudioVideo.style.borderRadius = "10px";
+
+    if(typeOfAudioVideo.classList.contains("type-of-audio-video")){
+        allAudioVideo.style.backgroundColor = "#DCF2FF"
+        allAudioVideo.style.marginLeft = "0px";
+        allAudioVideo.style.marginRight = "0px";
     }
 }
