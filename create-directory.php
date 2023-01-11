c852ec40c28ee4c23b94519f607592421133cab1
Victor Jauregui <vicjaudom@hotmail.com>
2023-01-11 18:07:29 +0100

    update victor

--- create-directory.php
+++ create-directory.php
@@ -4,10 +4,10 @@ $nameDirectory = $_REQUEST["name-directory"];
 
 
 $root = './root/'. $nameDirectory;
-$documents = './root/documents/'. $nameDirectory;
-$images = './root/images/nuevo';
-$audioVideo = './root/audio-video/nuevo';
-$trash = './root/trash/nuevo';
+$documents = './root/documents/' . $nameDirectory;
+$images = './root/images/nuevo' . $nameDirectory;
+$audioVideo = './root/audio-video/nuevo' . $nameDirectory;
+$trash = './root/trash/nuevo' . $nameDirectory;
 
 if (!file_exists($documents)) {
     mkdir($documents, 0777, true);
--- file.php
+++ file.php
@@ -2,24 +2,38 @@
 
 $name= $_FILES["file-upload"]["name"];
 $content = $_FILES["file-upload"]["tmp_name"];
-$archivo = "hola.png";
+$archivo = "./root/images/hola.png";
+$archivoName = $_REQUEST["file-upload"];
+
+$newFileImage = "./root/images/". $archivoName;
+$newFileDocument = "./root/documents/". $archivoName;
 
 
 
 
 if (strpos($name, 'jpeg') || strpos($name, 'png') || strpos($name, 'svg') !== false) {
     move_uploaded_file($content,"root/images/" .$name);
-    header("Location: index.php?createsuccesfully");
+    
 } else if (strpos($name, 'pptx') || strpos($name, 'word') || strpos($name, 'csv') || strpos($name, 'odt') ||  strpos($name, 'pdf') || strpos($name, 'txt') || strpos($name, 'exe') || strpos($name, 'zip') || (strpos($name, 'rar') !==false)) {
-    echo "document";
     move_uploaded_file($content,"root/documents/" .$name);
+    
 } else if(strpos($name, 'mp3') || strpos($name, 'mp4' !== false)){
-    echo "image or video";
+    move_uploaded_file($content,"root/audio-audio/" .$name);
 }
 
-if(file_exists($archivo)){
-    echo "si existe";
+
+if(file_exists($newFileImage)){
+    echo "You create this file: " . date("d F Y H:i:s.", filemtime($newFileImage)). "\n";
+    echo "You modifiqued last time this file: " . date("d F Y H:i:s.", filectime($newFileImage));
+} else if (file_exists($newFileDocument)){
+    echo "You create this file: " . date("d F Y H:i:s.", filemtime($newFileDocument)). "\n";
+    echo "You modifiqued last time this file: " . date("d F Y H:i:s.", filectime($newFileDocument));
 }
+
+
+
+
+
 
 
 ?>
