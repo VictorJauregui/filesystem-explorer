dcc9658548d6754975f8d83730e1980a6c024689
Victor Jauregui <vicjaudom@hotmail.com>
2023-01-11 13:25:22 +0100

    update

--- file.php
+++ file.php
@@ -5,25 +5,19 @@ $content = $_FILES["file-upload"]["tmp_name"];
 
 
 
-
-if(move_uploaded_file($content,"root/" .$name)){
-    echo "saved";
-
-} else{
-    echo"error";
-}
-
-
-if (!file_exists($images)) {
-    mkdir($images, 0777, true);
-    echo "está creada";
-}
-
-if (strpos($name, 'png') !== false) {
+if (strpos($name, 'png') || strpos($name, 'jpeg') || strpos($name, 'pptx') !== false) {
     echo "true";
     move_uploaded_file($content,"root/images/" .$name);
 
+} else {
+    echo "false";
+    move_uploaded_file($content,"root/documents/" .$name);
 }
+
+
+
+
+
 
 ?>
 
