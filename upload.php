cdf77d1272425c336bb4a54260592a0798e7cb11
Victor Jauregui <vicjaudom@hotmail.com>
2023-01-11 11:25:57 +0100

    update

--- upload.php
+++ upload.php
@@ -2,31 +2,31 @@
 <?php
 
 
-$name=$_FILES["Upload"]["name"];
-
-
+$name = $_FILES["Upload"]["name"];
 $content = $_FILES["Upload"]["tmp_name"];
 
 
-if(!file_exists("Upload")){
-    mkdir("Upload",0777,true);
+/*
+if(file_exists("root/".$name)){
+    echo "if";
+    mkdir("Giorgi",0777,true);
     if(file_exists("Upload")){
         if(move_uploaded_file($content,"root/".$name)){
             echo "saved";
         }else{
             echo"error";
         }
-
     }
 
-}else{
+}  
+else{
+    echo "else";*/
+
+
     if(move_uploaded_file($content,"root/".$name)){
         echo"saved";
     } else{
         echo"error";
     }
-}
-if($name="docx"){
-    move_uploaded_file($content,"root/documents/".$name);
-}
+
 ?>
