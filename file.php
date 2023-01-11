098fc79b118961d6452df9d0f2e2973b19d3e546
Victor Jauregui <vicjaudom@hotmail.com>
2023-01-11 13:47:47 +0100

    update

--- file.php
+++ file.php
@@ -4,17 +4,15 @@ $name= $_FILES["file-upload"]["name"];
 $content = $_FILES["file-upload"]["tmp_name"];
 
 
+function images(){}
 
-if (strpos($name, 'png') || strpos($name, 'jpeg') || strpos($name, 'pptx') !== false) {
+if (strpos($name, 'jpeg') || strpos($name, 'pptx') === true) {
     echo "true";
     move_uploaded_file($content,"root/images/" .$name);
-
-} else {
+} else if (strpos($name, 'png')) {
     echo "false";
-    move_uploaded_file($content,"root/documents/" .$name);
+    move_uploaded_file($content,"root/trash/" .$name);
 }
-
-
 
 
 
